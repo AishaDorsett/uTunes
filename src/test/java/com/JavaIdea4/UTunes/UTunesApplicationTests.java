2b24cde01e227c8443b0c8d8a9efcadd6ac678c8
Joe <josephshaw1563@gmail.com>
2021-11-15 14:41:42 +0000

    Project files

---
+++ src/test/java/com/JavaIdea4/UTunes/UTunesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaIdea4.UTunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UTunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
