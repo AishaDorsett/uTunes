cd249626baaa66e121fd5056086ae8c0645cded6
Matthew Read <matthewreadbass@hotmail.co.uk>
2021-11-16 16:29:27 +0000

    added search button

--- src/main/java/com/JavaIdea4/UTunes/controller/TuneController.java
+++ src/main/java/com/JavaIdea4/UTunes/controller/TuneController.java
@@ -1,7 +1,12 @@
 package com.JavaIdea4.UTunes.controller;
 
+import com.JavaIdea4.UTunes.model.Tune;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class TuneController {
@@ -13,4 +18,8 @@ public class TuneController {
 		// return search;
 	}
 
+	@PostMapping("/search")
+	public RedirectView create(@ModelAttribute Tune tune) {
+		return new RedirectView("/search");
+	}
 }
