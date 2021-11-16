d14b1607082fac2f0b33fa6a9ed0b889095fe678
Matthew Read <matthewreadbass@hotmail.co.uk>
2021-11-16 16:46:43 +0000

    ttfiguring out how to search by genre

--- src/main/java/com/JavaIdea4/UTunes/controller/TuneController.java
+++ src/main/java/com/JavaIdea4/UTunes/controller/TuneController.java
@@ -1,7 +1,9 @@
 package com.JavaIdea4.UTunes.controller;
 
 import com.JavaIdea4.UTunes.model.Tune;
+import com.JavaIdea4.UTunes.repository.TuneRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,6 +13,9 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 public class TuneController {
 
+	@Autowired
+	TuneRepository repository;
+
 	@GetMapping("/search")
 	public String index() {
 		// String search = "Search";
@@ -19,7 +24,8 @@ public class TuneController {
 	}
 
 	@PostMapping("/search")
-	public RedirectView create(@ModelAttribute Tune tune) {
+	public RedirectView create(@ModelAttribute String tune) {
+		Iterable<Tune> tunes = repository.findByGenre(tune);
 		return new RedirectView("/search");
 	}
 }
