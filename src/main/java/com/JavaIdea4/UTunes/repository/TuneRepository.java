0dc049f3fc8eb41405fbb0eb04ef3a881cff995f
Joe <josephshaw1563@gmail.com>
2021-11-16 12:05:57 +0000

    bugs

--- src/main/java/com/JavaIdea4/UTunes/repository/TuneRepository.java
+++ src/main/java/com/JavaIdea4/UTunes/repository/TuneRepository.java
@@ -1,6 +1,6 @@
 package com.JavaIdea4.UTunes.repository;
 
-import com.JavaIdea4.UTunes.model.Genre;
+import com.JavaIdea4.UTunes.model.Tune;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 
