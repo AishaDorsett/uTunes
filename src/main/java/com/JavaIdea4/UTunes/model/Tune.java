296c4e71c7e9866f871e5777cb66b048eb033aa9
Joe <josephshaw1563@gmail.com>
2021-11-16 12:04:07 +0000

    fixed file

---
+++ src/main/java/com/JavaIdea4/UTunes/model/Tune.java
@@ -0,0 +1,66 @@
+package com.JavaIdea4.UTunes.model;
+
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tunes")
+public class Tune {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(name = "trackname")
+	private String trackname;
+
+	@Column(name = "artistname")
+	private String artistname;
+
+	@Column(name = "genre")
+	private String genre;
+
+	public Tune() {
+
+	}
+
+	public Tune(String title, String artistname, String genre) {
+		this.trackname = trackname;
+		this.artistname = artistname;
+		this.genre = genre;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getTrackname() {
+		return trackname;
+	}
+
+	public void setTrackname(String trackname) {
+		this.trackname = trackname;
+	}
+
+	public String getArtistname() {
+		return artistname;
+	}
+
+	public void setArtistname(String artistname) {
+		this.artistname = artistname;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	@Override
+	public String toString() {
+		return "Tutorial [id=" + id + ", Trackname=" + trackname + ", Artist=" + artistname + ", Genre=" + genre + "]";
+	}
+}
