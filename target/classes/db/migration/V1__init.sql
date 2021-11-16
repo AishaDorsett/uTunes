8fef26cc0cf00ecd63d326f4fe4dd7a4fffbe94e
Matthew Read <80341862+matthewreadbass@users.noreply.github.com>
2021-11-16 15:43:50 +0000

    Delete target directory

--- target/classes/db/migration/V1__init.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS tunes;
-
-CREATE TABLE tunes (
-  id bigserial PRIMARY KEY,
-  trackname varchar(250) NOT NULL,
-  artistname varchar(250) NOT NULL,
-  genre varchar(250) NOT NULL
-);
